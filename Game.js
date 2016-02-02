1121ebe3aedb3cc6566544f046ae7d9ad0b0babf
Sam Quinn <lazersmoke@gmail.com>
2016-02-02 17:56:46 -0600

    Fixed syntax errors part 1

--- Game.js
+++ Game.js
@@ -2,14 +2,14 @@ var Game = function (canvasId, tileSize, width, height) { // Master class
 	this.map = new Map(canvasId, tileSize, width, height) //Creates map object
 	this.tileArray = []
 	this.nodeArray = [] //Tells enemies where to go
-	this.reset()
+	this.resetGame()
 }
 
-Game.protoype.reset = function () { //Resets Game
+Game.prototype.resetGame = function () { //Resets Game
 	this.buildMap()
 }  
 
-Game.protoype.buildMap = function() {
+Game.prototype.buildMap = function() {/*
 	for (var x = 0; x < this.width; x++) {
 		this.tileArray[x] = []
 		for (var y = 0; y < this.height; y++) {
@@ -25,5 +25,5 @@ Game.protoype.buildMap = function() {
 	for (var i = 9; i < 15; i++) {
 		this.tileArray[4][i] = "path"
 	}
-	this.nodeArray = [[12, 0], [12, 8], [4, 8], [4, 14]] //Sets location of nodes
+	this.nodeArray = [[12, 0], [12, 8], [4, 8], [4, 14]] //Sets location of nodes*/
 }  
