cf558bb5e65d737f2342feb31dadcf314712ade9
Chaotic-Potato <andy825825@gmail.com>
2016-02-02 20:23:43 -0600

    Change array name so Sam stops bitching

--- Enemies.js
+++ Enemies.js
@@ -1,17 +1,17 @@
 var Enemies =  function () {
-	this.array = []
+	this.enemiesArray = []
 }
 
 Enemies.prototype.spawnEnemy(type, x, y, nodeArray, speed) {
-	this.array.push(new Enemy(type, x, y, nodeArray, speed))
+	this.enemiesArray.push(new Enemy(type, x, y, nodeArray, speed))
 }
 
 Enemies.prototype.killEnemy(id) {
-	this.array.splice(this.array.indexOf(id), 1)
+	this.enemiesArray.splice(this.enemiesArray.indexOf(id), 1)
 }
 
 Enemies.prototype.enemiesTick() {
-	for (i in array) {
-		this.array[i].enemyTick()
+	for (i in enemiesArray) {
+		this.enemiesArray[i].enemyTick()
 	}
 }
