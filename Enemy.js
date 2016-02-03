8a72efa8275cc9051fa76c7d7ae59967a1284063
Chaotic-Potato <andy825825@gmail.com>
2016-02-02 21:39:27 -0600

    Uses tileToPixel now

--- Enemy.js
+++ Enemy.js
@@ -10,33 +10,35 @@ var Enemy = function(type, x, y, nodeArray, speed) { // Constructer
 Enemy.prototype = {
 	constructor: Enemy,
 	enemyTick: function () { //Tick
-		if (this.x != this.nodeArray[this.nodeNum][0] * game.tileSize) { //If X need to change
-			if (Math.abs(this.x - this.nodeArray[this.nodeNum][0] * game.tileSize) < this.speed) { //If X is closer than speed
+		alert("Enemy Tick")
+		alert("Enemy Tick")
+		if (this.x != tileToPixel(this.nodeArray[this.nodeNum][0])) { //If X need to change
+			if (Math.abs(this.x - tileToPixel(this.nodeArray[this.nodeNum][0]) < this.speed) { //If X is closer than speed
 				this.x = this.nodeArray[this.nodeNum][0] * game.tileSize
 			}
 			else {
-				if (this.x < this.nodeArray[this.nodeNum][0] * game.tileSize) { //Move X up
+				if (this.x < this.nodeArray[tileToPixel(this.nodeArray[this.nodeNum][0])) { //Move X up
 					this.x += speed
 				}
-				if (this.x > this.nodeArray[this.nodeNum][0] * game.tileSize) { //Move X down
+				if (this.x > this.nodeArray[tileToPixel(this.nodeArray[this.nodeNum][0])) { //Move X down
 					this.x -= speed
 				}
 			}
 		}
 		if (this.y != this.nodeArray[this.nodeNum][1] * game.tileSize) { //If Y need to change
-			if (Math.abs(this.y - this.nodeArray[this.nodeNum][1] * game.tileSize) < this.speed) { //If Y is closer than speed
+			if (Math.abs(tileToPixel(this.y - this.nodeArray[this.nodeNum][1])) < this.speed) { //If Y is closer than speed
 				this.y = this.nodeArray[this.nodeNum][1] * game.tileSize
 			}
 			else {
-				if (this.y < this.nodeArray[this.nodeNum][1] * game.tileSize) { //Move Y up
+				if (this.y < this.nodeArray[tileToPixel(this.nodeNum][1])) { //Move Y up
 					this.y += speed
 				}
-				if (this.y > this.nodeArray[this.nodeNum][1] * game.tileSize) { //Move Y down
+				if (this.y > this.nodeArray[tileToPixel(this.nodeNum][1])) { //Move Y down
 					this.y -= speed
 				}
 			}
 		}
-		if (this.x == this.nodeArray[this.nodeNum][0] * game.tileSize && this.y == this.nodeArray[this.nodeNum][1] * game.tileSize) { // If at node
+		if (this.x == tileToPixel(this.nodeArray[this.nodeNum][0]) && this.y == tileToPixel(this.nodeArray[this.nodeNum][1])) { // If at node
 			this.nodeNum++
 		}
 	}
