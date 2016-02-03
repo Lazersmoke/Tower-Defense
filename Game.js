df1be48994ca066652489a8ec94ab9c84f219670
Chaotic-Potato <andy825825@gmail.com>
2016-02-02 20:44:40 -0600

    This Fix

--- Game.js
+++ Game.js
@@ -3,7 +3,7 @@ var Game = function (canvasId, tileSize, width, height) { // Master class
 	this.resetGame()
 	this.tickrate = 25
 	this.enemies = new Enemies()
-	this.enemies.spawnEnemy ("John Cena", map.nodeArray[0][0], map.nodeArray[0][1], map.nodeArray, 5)
+	this.enemies.spawnEnemy ("John Cena", this.map.nodeArray[0][0], this.map.nodeArray[0][1], this.map.nodeArray, 5)
 }
 
 Game.prototype = {
