eccd709e4aa04a107ff839b79b1078ed48ab0548
Sam Quinn <lazersmoke@gmail.com>
2016-02-02 21:16:41 -0600

    FIXED

--- Game.js
+++ Game.js
@@ -3,7 +3,6 @@ var Game = function (canvasId, tileSize, width, height) { // Master class
 	this.resetGame()
 	this.tickrate = 25
 	this.enemies = new Enemies()
-	this.enemies.spawnEnemy ("John Cena", tileToPixel(this.map.nodeArray[0][0]), tileToPixel(this.map.nodeArray[0][1]), this.map.nodeArray, 5)
 }
 
 Game.prototype = {
