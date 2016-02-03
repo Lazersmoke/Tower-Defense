513b850abd79b62c304a70dad7215fd538d0bbf3
Chaotic-Potato <andy825825@gmail.com>
2016-02-02 20:29:20 -0600

    Fix the derp

--- Enemies.js
+++ Enemies.js
@@ -2,16 +2,17 @@ var Enemies =  function () {
 	this.enemiesArray = []
 }
 
-Enemies.prototype.spawnEnemy(type, x, y, nodeArray, speed) {
-	this.enemiesArray.push(new Enemy(type, x, y, nodeArray, speed))
-}
-
-Enemies.prototype.killEnemy(id) {
-	this.enemiesArray.splice(this.enemiesArray.indexOf(id), 1)
-}
-
-Enemies.prototype.enemiesTick() {
-	for (i in enemiesArray) {
-		this.enemiesArray[i].enemyTick()
+Enemies.prototype = {
+	constructer: Enemies,
+	spawnEnemy: function (type, x, y, nodeArray, speed) {
+		this.enemiesArray.push(new Enemy(type, x, y, nodeArray, speed))
+	}
+	killEnemy: function (id) {
+		this.enemiesArray.splice(this.enemiesArray.indexOf(id), 1)
+	}
+	enemiesTick: function () {
+		for (i in enemiesArray) {
+			this.enemiesArray[i].enemyTick()
+		}
 	}
 }
