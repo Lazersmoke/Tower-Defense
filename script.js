e750478198aa343c18b1b233f1d96bab771130e7
Sam Quinn <lazersmoke@gmail.com>
2016-02-02 18:39:19 -0600

    Refactored out prototype

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
 game = new Game("map", 64, 15, 15)
 game.map.drawTiles()
-theMap = game.map
-tickrate = 25;
-
-function tick(){
-	preTick();
-	enemiesTick();
-	towersTick();
-	UITick();
-	postTick();
-}
-
-function preTick(){
-	
-}
-
-function postTick(){
-	
-}
