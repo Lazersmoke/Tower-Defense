8d09766439360239cb266919d45d9594355e5d8f
Sam Quinn <lazersmoke@gmail.com>
2016-02-02 20:43:41 -0600

    Update

--- BasicTower.js
+++ BasicTower.js
@@ -1,5 +1,5 @@
 var BasicTower = function(tileX, tileY) {
-	Tower.call(this, "Basic Tower", tileX, tileY);
+	Tower.call(this, "Basic Tower", 25, tileX, tileY);
 }
 BasicTower.prototype = Object.create(Tower.prototype);
 BasicTower.prototype.constructor = BasicTower;
--- Tower.js
+++ Tower.js
@@ -1,7 +1,9 @@
-var Tower = function(name, tileX, tileY) {
+var Tower = function(name, fireSpeed, tileX, tileY) {
 	this.name = name;
+	this.fireSpeed = fireSpeed;//In ticks of cooldown
 	this.tileX = tileX;
 	this.tileY = tileY;
+	this.cooldown = 0;
 	Tower.towerList.push(this);
 }
 Tower.towerList = [];
@@ -18,9 +20,31 @@ Tower.tickTowers = function () {
 Tower.prototype = {
 	constructor: Tower,
 	tickTower: function () {
+		this.shoot()
 		console.log(this.name + " got Ticked!");//Fire at enemies
 	},
 	renderTower: function () {
 		console.log(this.name + " got Rendered!");//Fire at enemies
+	},
+	shoot: function () {
+		if(this.cooldown > 0){
+			this.cooldown--
+		}
+		var currCloseset = -1;
+		var currClosesetDist = 100000;
+		Enemies.array.forEach(function(a, b){
+			if(distance(a.x,a.y,tileToPixel(this.TileX),tileToPixel(this.TileY)) < currClosestDist){
+				currClosestDist = distance(a.x,a.y,tileToPixel(this.TileX),tileToPixel(this.TileY))
+				currClosest = b;
+			}
+		});
+		if(currClosest > -1 && this.cooldown == 0){
+			Enemies.killEnemy(currCloseset);
+			this.cooldown = this.fireSpeed
+		}
 	}
+}
+
+function distance(xa,ya,xb,yb){
+	Math.sqrt(Math.pow((xa-xb),2)+Math.pow((ya-yb),2))
 }
