73778479e0100ef5cd84d44300172e42acd5b034
Chaotic-Potato <andy825825@gmail.com>
2016-02-02 20:37:30 -0600

    Add Tile and Pixel Conversion

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 game = new Game("map", 64, 15, 15)
 game.map.drawTiles()
+
+function tileToPixel(cord) {
+  return (cord + 0.5) * game.tileSize
+}
+
+function pixelToTile(cord) {
+  return Math.abs((cord / game.tileSize) - 0.5)
+}
