a82108927865449f579cbc307c75aecea054f612
Sam Quinn <lazersmoke@gmail.com>
2016-02-06 11:26:05 -0600

    fixed towers not placing

--- input/InputHandler.js
+++ input/InputHandler.js
@@ -4,13 +4,16 @@ game.input.InputHandler = {
 			var x = evt.offsetX
 			var y = evt.offsetY
 			if ($Game.removeMoney(100)) {
+				console.log("placing tower")
 				if($Tower.validLocation(new $TilePos($Map.pixelToTile(x), $Map.pixelToTile(y)))){
 					new $BasicTower(new $TilePos($Map.pixelToTile(x), $Map.pixelToTile(y)))
+					return
 				}
-				else if($Tower.towerAt(new $TilePos($Map.pixelToTile(x), $Map.pixelToTile(y))) != null){
-					$Tower.towerAt(new $TilePos($Map.pixelToTile(x), $Map.pixelToTile(y))).removeTower()
-				}	
 			}
+			if($Tower.towerAt(new $TilePos($Map.pixelToTile(x), $Map.pixelToTile(y))) != null){
+				$Tower.towerAt(new $TilePos($Map.pixelToTile(x), $Map.pixelToTile(y))).removeTower()
+				$Game.addMoney(75)
+			}	
 		}
 	}
 }
